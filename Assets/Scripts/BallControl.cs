419366258bd2d0c613011a9c294b34e8f5053a1e
yazedhaidar <88135665+yazedhaidar@users.noreply.github.com>
2021-10-09 22:29:32 +0700

    problem 2

---
+++ Assets/Scripts/BallControl.cs
@@ -0,0 +1,65 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BallControl : MonoBehaviour
+{
+    // Rigidbody 2D bola
+    private Rigidbody2D rigidBody2D;
+
+    // Besarnya gaya awal yang diberikan untuk mendorong bola
+    public float xInitialForce;
+    public float yInitialForce;
+
+    void ResetBall()
+    {
+        //ubah posisi bola jadi(0.0)
+        transform.position = Vector2.zero;
+
+        // Reset kecepatan menjadi (0,0)
+        rigidBody2D.velocity = Vector2.zero;
+    }
+
+    void PushBall()
+    {
+     
+
+        // Tentukan nilai acak antara 0 (inklusif) dan 2 (eksklusif)
+        float randomDirection = Random.Range(0, 2);
+
+        // Jika nilainya di bawah 1, bola bergerak ke kiri. 
+        // Jika tidak, bola bergerak ke kanan.
+        if (randomDirection < 1.0f)
+        {
+            // Gunakan gaya untuk menggerakkan bola ini.
+            rigidBody2D.AddForce(new Vector2(-xInitialForce, yInitialForce));
+        }
+        else
+        {
+            rigidBody2D.AddForce(new Vector2(xInitialForce, yInitialForce));
+        }
+    }
+
+    void restart()
+    {
+        // Kembalikan bola ke posisi semula
+        ResetBall();
+
+        // Setelah 1 detik, berikan gaya ke bola
+        Invoke("PushBall", 1);
+    }
+    // Start is called before the first frame update
+    void Start()
+    {
+        rigidBody2D = GetComponent<Rigidbody2D>();
+
+        //mulai game
+        restart();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
