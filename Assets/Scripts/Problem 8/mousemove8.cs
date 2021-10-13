92edb7a7c5151d520daee1b760217e9640b7e840
yazedhaidar <88135665+yazedhaidar@users.noreply.github.com>
2021-10-13 07:19:35 +0700

    Update

---
+++ Assets/Scripts/Problem 8/mousemove8.cs
@@ -0,0 +1,126 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class mousemove8 : MonoBehaviour
+{
+
+
+    // Batas atas dan bawah game scene (Batas bawah menggunakan minus (-))
+    public static float yBoundary = 9.0f;
+
+    // Batas kiri dan kanan game scene (Batas bawah menggunakan minus (-))
+    public static float xBoundary = 16.8f;
+
+
+    // Rigidbody 2D bola ini
+    private Rigidbody2D rigidBody2D;
+
+    public Score7 Score;
+
+    public int addscore = 0;
+    public bool check = false;
+    public spawn2 Spawn;
+    float Timer = 0f;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        rigidBody2D = GetComponent<Rigidbody2D>();
+
+    }
+
+
+    // Update is called once per frame
+    void Update()
+    {
+
+        Vector2 CursorPos = Camera.main.ScreenToWorldPoint(Input.mousePosition);
+        transform.position = new Vector2(CursorPos.x, CursorPos.y);
+
+        if (CursorPos.x > xBoundary)
+        {
+            CursorPos.x = xBoundary;
+
+            if (CursorPos.y > yBoundary)
+            {
+                CursorPos.y = yBoundary;
+            }
+            else if (CursorPos.y < -yBoundary)
+            {
+                CursorPos.y = -yBoundary;
+            }
+
+            transform.position = new Vector2(CursorPos.x, CursorPos.y);
+        }
+        else if (CursorPos.y > yBoundary)
+        {
+            CursorPos.y = yBoundary;
+
+            if (CursorPos.x > xBoundary)
+            {
+                CursorPos.x = xBoundary;
+            }
+            else if (CursorPos.x < -xBoundary)
+            {
+                CursorPos.x = -xBoundary;
+            }
+
+            transform.position = new Vector2(CursorPos.x, CursorPos.y);
+        }
+        else if (CursorPos.x < -xBoundary)
+        {
+            CursorPos.x = -xBoundary;
+
+            if (CursorPos.y > yBoundary)
+            {
+                CursorPos.y = yBoundary;
+            }
+            else if (CursorPos.y < -yBoundary)
+            {
+                CursorPos.y = -yBoundary;
+            }
+
+            transform.position = new Vector2(CursorPos.x, CursorPos.y);
+        }
+        else if (CursorPos.y < -yBoundary)
+        {
+            CursorPos.y = -yBoundary;
+
+            if (CursorPos.x > xBoundary)
+            {
+                CursorPos.x = xBoundary;
+            }
+            else if (CursorPos.x < -xBoundary)
+            {
+                CursorPos.x = -xBoundary;
+            }
+
+            transform.position = new Vector2(CursorPos.x, CursorPos.y);
+        }
+
+        if (check == true)
+        {
+            
+            Timer += Time.deltaTime;
+            if (Timer >= Spawn.spawnTime)
+            {
+                Spawn.Spawn();
+                Timer = 0f;
+                check = false;
+            }
+        }
+
+
+    }
+
+
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        Destroy(collision.gameObject);
+
+        Score.IncreaseCurrentScore(addscore);
+        check = true;
+
+    }
+}
--- Assets/Scripts/Problem 8/spawn2.cs
+++ Assets/Scripts/Problem 8/spawn2.cs
@@ -7,20 +7,27 @@ public class spawn2 : MonoBehaviour
     public GameObject[] box;
     public float spawnTime = 3f;
     public Transform[] spawnPoints;
+    public Collider2D[ Collider;
+    
 
     void Start()
     {
         //Mengeksekusi fungs Spawn setiap beberapa detik sesui dengan nilai spawnTime
         Spawn();
         Spawn();
-        InvokeRepeating("Spawn", spawnTime, spawnTime);
+        
+       
     }
 
-
-    void Spawn()
+    void Update()
+    {
+      
+    }
+    public void Spawn()
     {
         Vector3 BoxPosition = new Vector3(Random.Range(-15.5f, 15.5f), Random.Range(-6.5f, 6.5f), 0f);
 
+       
         //Mendapatkan nilai random
         int spawnPointIndex = Random.Range(0, spawnPoints.Length);
         int randomIndex = Random.Range(0, box.Length);
